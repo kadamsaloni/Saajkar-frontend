0e7b5dcc071ca43342420fb7a00944b200f12d3d
kadamsaloni <kadamsaloni26@gmail.com>
2026-07-27 22:55:32 +0530

    Login and register done

--- frontend/Saajkar/src/pages/Login.jsx
+++ frontend/Saajkar/src/pages/Login.jsx
@@ -1,112 +1,53 @@
 import "./Login.css";
-
-import logo from "../assets/saajkar-logo.png";
-
+import registerLogo from "../assets/register-logo.jpg";
 import { useNavigate, Link } from "react-router-dom";
 
-
-
 function Login() {
-
   const navigate = useNavigate();
 
-
-
   return (
-
     <div className="container">
-
-
-
       <div className="login-card">
 
-
-
-        <img src={logo} alt="Saajkar" className="logo" />
-
-
-
-
-
-
-        <p className="tagline">
-
-         
-
-        </p>
-
-
+        <img
+          src={registerLogo}
+          alt="Login"
+          className="top-image"
+        />
 
         <input
-
           type="email"
-
           placeholder="Email Address"
-
         />
-
-
 
         <input
-
           type="password"
-
           placeholder="Password"
-
         />
-
-
 
         <div className="forgot">
-
           <a href="#">Forgot Password?</a>
-
         </div>
-
-
 
         <button className="login-btn">
-
           Login
-
         </button>
-
-
 
         <button
-
           className="home-btn"
-
           onClick={() => navigate("/")}
-
         >
-
           Back to Home
-
         </button>
 
-
-
         <div className="register">
-
           Don't have an account?{" "}
-
           <Link to="/register">Register</Link>
-
         </div>
 
-
-
       </div>
-
-
-
     </div>
-
   );
-
 }
-
-
 
 export default Login;
